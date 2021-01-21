1f53139b7aa024e8eb1a4d5a1fd92b1247442c0d
Jakub Kraus <poczta.jakub1@gmail.com>
2021-01-21 19:57:22 +0100

    Added RWD

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Game from "./game";
 import "./index.css";
+import "./rwd.css";
 
 ReactDOM.render(<Game />, document.getElementById("root"));
