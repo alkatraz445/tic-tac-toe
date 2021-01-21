d9cbf752a4bec8f33e1201a366358f5a349314c4
Jakub Kraus <poczta.jakub1@gmail.com>
2021-01-21 21:28:20 +0100

    Temporarily disabled eslint

--- src/square.js
+++ src/square.js
@@ -1,6 +1,8 @@
 const Square = (props) => {
   const winStyle =
+    /* eslint-disable */
     "square" + " " + (props.highlight ? "highlight" : " ") + " " + "flex";
+  /* eslint-enable */
   return (
     <div className={winStyle} onClick={props.onClick}>
       {props.value}
